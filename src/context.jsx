8b161cf6bee56d9d3d7b1dac3895b822465c76c6
Sekamatte Soul Solomon <99466179+SoulSolomon21@users.noreply.github.com>
2022-09-11 13:40:57 +0000

    added useEffect to fetch meal data from api

--- src/context.jsx
+++ src/context.jsx
@@ -1,12 +1,33 @@
 import React, {useContext, useEffect} from "react";
 
 const AppContext = React.createContext()
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const AppProvider = ({children}) => {
 
+  // useEffect(() => {
+  //   console.log("fetch some data")
+  //   fetch('https://randomuser.me/api/').then(
+  //     (data) => {
+  //       console.log(data.json())
+  //     }
+  //   )
+  // }, [])
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/')
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
   useEffect(() => {
-    console.log("fetch some data")
-  }, [])
+    fetchData()
+  },[])
   
   return <AppContext.Provider value={{name:"soul", occupation:"student"}}>
     {children}
@@ -17,4 +38,4 @@ export const useGlobalContext = () => {
   return useContext(AppContext)
 }
 
-export { AppContext,AppProvider }
+export { AppContext,AppProvider, allMealsUrl, randomMealUrl }
