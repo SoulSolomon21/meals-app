378c8681d9f58922f2c2bda7017198e50de6f54d
Sekamatte Soul Solomon <99466179+SoulSolomon21@users.noreply.github.com>
2022-09-12 06:01:24 +0000

    applied styling to image component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -5,14 +5,14 @@ const Meals = () => {
 const {meals} = useGlobalContext()
   
   return(
-    <section>
+    <section  className="section-center">
       {meals.map((meal) => {
       const {idMeal, strMeal:title, strMealThumb: image} = meal
-      return <article key={idMeal}>
-        <img src={image} />
+      return <article className="single-meal" key={idMeal}>
+        <img src={image} className="img" style={{width:'200px'}}/>
           <footer>
             <h5>{title}</h5>
-            <button>click me</button>
+            <button className="like-btn">click me</button>
           </footer>
       </article>
       })
